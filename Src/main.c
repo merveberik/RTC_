6fd528b8c8f51d040afe7f2fa4331fc01a0e43a5
merveberik <54530863+merve-cpu@users.noreply.github.com>
2019-12-16 14:28:32 +0300

    RTC_ time and date adjust. you don't use reset button or power reset

--- Src/main.c
+++ Src/main.c
@@ -30,7 +30,7 @@ void set_time (void){
 	sDate.Year = 19;
 	HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BIN);
 
-//	HAL_RTCEx_BKUPWrite(&hrtc, RTC_BKP_DR0, 0x32F2);
+//	HAL_RTCEx_BKUPWrite(&hrtc, RTC_BKP_DR0, 0x32F2); //if we use reset button or power reset, it can be aktive
 }
 
 int main(void){
